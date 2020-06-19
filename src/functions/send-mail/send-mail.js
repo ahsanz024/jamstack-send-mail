040a45fb89c17364948e13ff8af96cd8a9223e60
Ahsan <ahsan024@gmail.com>
2020-06-19 15:23:24 +0200

    moving functions to src folder + adding send-mail function

---
+++ src/functions/send-mail/send-mail.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+exports.handler = async (data, _context, callback) => {
+  const mailgun = require("mailgun-js");
+
+  const mg = mailgun({
+    apiKey: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_DOMAIN,
+  });
+
+  const json = JSON.stringify(data);
+
+  const mailObject = {
+    from: `Contact Form <postmaster@${process.env.MAILGUN_DOMAIN}>`,
+    to: `${process.env.RECIPIENT}`,
+    subject: `From: ${json.name} + Company: ${json.company}`,
+    text: `
+      Email: ${json.email} \n
+      Message: ${json.message}
+    `,
+  };
+
+  try {
+    const resp = await mg.messages().send(mailObject);
+    return {
+      statusCode: 200,
+      body: "Your message was sent successfully! We'll be in touch.",
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      statusCode: 500,
+      body: "Problem sending mail",
+    };
+  }
+};
